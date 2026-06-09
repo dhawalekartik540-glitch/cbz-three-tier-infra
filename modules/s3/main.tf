ba20059f40d8eab492c45d30f1917931d295db80
dhawalekartik540-glitch <dhawalekartik540@gmail.com>
2026-06-09 16:47:56 +0530

    gg

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,31 +1,31 @@
 # Create an S3 bucket
-resource "aws_s3_bucket" "cbz_bucket6666262" {
-  bucket = "cbz-frontend-project-bux" # Must be globally unique
+resource "aws_s3_bucket" "cbz_bucket" {
+  bucket = "cbz-frontend-project-bux" # Replace with a globally unique bucket name
 
+  # Enable static website hosting
   website {
     index_document = "index.html"
     error_document = "error.html"
   }
 
   tags = {
-    Name = "StaticWebsiteBucket"
-    env  = "dev"
+    Name        = "StaticWebsiteBucket"
+    env = "dev"
   }
 }
 
 # Disable Block Public Access
 resource "aws_s3_bucket_public_access_block" "example" {
-  bucket = aws_s3_bucket.cbz_bucket6666262.id
+  bucket = aws_s3_bucket.cbz_bucket.bucket
 
   block_public_acls       = false
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
 }
-
-# Bucket policy for public website access
+# Set the bucket policy to allow public read access (use cautiously)
 resource "aws_s3_bucket_policy" "static_website_policy" {
-  bucket = aws_s3_bucket.cbz_bucket6666262.id
+  bucket = aws_s3_bucket.cbz_bucket.id
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -34,18 +34,15 @@ resource "aws_s3_bucket_policy" "static_website_policy" {
         Effect    = "Allow"
         Principal = "*"
         Action    = "s3:GetObject"
-        Resource  = "${aws_s3_bucket.cbz_bucket6666262.arn}/*"
+        Resource  = "${aws_s3_bucket.cbz_bucket.arn}/*"
       }
     ]
   })
-
-  depends_on = [
-    aws_s3_bucket_public_access_block.example
-  ]
+  depends_on = [aws_s3_bucket_public_access_block.example]
 }
 
-# Output website endpoint
+# Output the bucket's website endpoint
 output "website_endpoint" {
-  value       = aws_s3_bucket.cbz_bucket6666262.website_endpoint
+  value       = aws_s3_bucket.cbz_bucket.website_endpoint
   description = "The URL to access the static website"
 }
